8c06b9e34cec2045a4abe2438f7777fb4c3ed741
dhvanikanaik523 <dhvanikanaik2003@gmail.com>
2025-11-10 01:52:00 +0530

    Connect to live backend

--- backend/index.js
+++ backend/index.js
@@ -45,20 +45,20 @@ const cron = require('node-cron');
 //   database: process.env.DB_NAME,
 // });
 
-// const pool = new Pool({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
-
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
 
 module.exports = pool;
 
